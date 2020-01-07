6a5668e9652f63e1df318cad1d30e317089b25b3
Steven <anguelr12@hotmail.com>
2020-01-07 11:51:12 -0500

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  
+  nombre:string = "Angel";
+
+  arreglo:number[] = [1,2,3,4,5,6,7,8,9,10];
+
+  PI = Math.PI;
+
+  a:number = 0.234;
+
+  salario:number = 1234.5;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion:{
+      calle: "Wolf",
+      casa: "55"
+    }
+  }
+
+  valorDePromesa = new Promise((resolve, reject) =>{
+    setTimeout(()=>resolve('Llego la data!'), 3500);
+  })
 }
